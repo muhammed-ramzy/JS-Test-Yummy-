b15a89efeb80fa298890e6ceb27a0e02e5a26f6f
muhammed-ramzy <muhammedabdelfattah89@gmail.com>
2024-07-14 14:30:49 +0300

    initial commit

---
+++ JS/index.js
@@ -0,0 +1,149 @@
+let meals = $("#meals-section")
+let categories = $("#categories-section")
+let area = $("#area-section")
+let ingredients = $("#ingredients-section")
+
+let navListItems = $(".nav-list li")
+
+
+
+//navigating using the navigation list
+navListItems.on("click", (eventInfo) =>
+{
+    //get the id of each one
+    //add # and -section to it
+    let sectionId = "#" + $(eventInfo.target).attr("id") + "-section";
+
+
+    //removeClass d-none from the result section
+    $(sectionId).removeClass("d-none");
+    $("#meals-section").addClass("d-none");
+
+    //addClass d-none to the rest sections
+    for (let i = 0; i < navListItems.length; i++) {
+        if(`#${navListItems.eq(i).attr("id")}-section` !== sectionId)
+        {
+            $(`#${navListItems.eq(i).attr("id")}-section`).addClass("d-none")
+        }
+    }
+})
+
+// side bar toggle
+$("#burger-icon").on("click", () => {
+    if ($("aside").css("left") == '-259.2px') {
+        $("aside").animate({ "left": "0rem" }, () => {
+            $("#search").animate({ "top": "0px" }, 500)
+            $("#categories").animate({ "top": "0px" }, 600)
+            $("#area").animate({ "top": "0px" }, 700)
+            $("#ingredients").animate({ "top": "0px" }, 800)
+            $("#contact-us").animate({ "top": "0px" }, 900)
+        })
+        $("#burger-icon").removeClass("fa-align-justify")
+        $("#burger-icon").addClass("fa-x")
+    }
+    else {
+        $("aside").animate({ "left": "-16.2rem" })
+        $("#search").animate({ "top": "208px" }, 400)
+        $("#categories").animate({ "top": "208px" }, 400)
+        $("#area").animate({ "top": "208px" }, 400)
+        $("#ingredients").animate({ "top": "208px" }, 400)
+        $("#contact-us").animate({ "top": "208px" }, 400)
+
+        $("#burger-icon").addClass("fa-align-justify")
+        $("#burger-icon").removeClass("fa-x")
+    }
+})
+
+
+//The main API
+async function getAllMeals()
+{
+    let fetchedMeals = await fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=");
+    let response = await fetchedMeals.json();
+
+    let blackBox = ``;
+    for (let i = 0; i < 20; i++) {
+        blackBox += `<div class="col-md-3">
+                <div class="card position-relative border-0 overflow-hidden">
+                    <img src="${response.meals[i].strMealThumb
+                    }" class="h-100" alt="">
+                    <div class="layer position-absolute d-flex align-items-center">
+                        <h3 class="text-black m-0">${response.meals[i].strMeal}</h3>
+                    </div>
+                </div>
+            </div>`
+        
+    }
+    meals.html(blackBox)
+}
+async function getArea()
+{
+    let fetchedCategories = await fetch("https://www.themealdb.com/api/json/v1/1/list.php?a=list");
+    let response = await fetchedCategories.json();
+
+    console.log(response);
+    
+    let blackBox = ``;
+    for (let i = 0; i < response.meals.length; i++) {
+        blackBox += `<div class="col-md-3">
+                <div class="card bg-transparent border-0 d-flex justify-content-center align-items-center text-white">
+                    <i class="fa-solid fa-house-laptop fa-4x"></i>
+                    <h3>${response.meals[i].strArea}</h3>
+                </div>
+            </div>`;
+        
+    }
+    area.html(blackBox)
+}
+async function getIngredients()
+{
+    let fetchedIngredients = await fetch("https://www.themealdb.com/api/json/v1/1/list.php?i=list");
+    let response = await fetchedIngredients.json();
+
+    console.log(response);
+    console.log(response.meals.length);
+    console.log(response.meals[0].strDescription);
+    
+    let blackBox = ``;
+    for (let i = 0; i < 20; i++) {
+        blackBox += `<div class="col-md-3">
+                <div
+                    class="card bg-transparent border-0 d-flex justify-content-center align-items-center text-white text-center">
+                    <i class="fa-solid fa-drumstick-bite fa-4x"></i>
+                    <h3>${response.meals[i].strIngredient}</h3>
+                    <p>${response.meals[i].strDescription.split(" ").slice(0, 20).join(" ")}</p>
+                </div>
+            </div>`;
+        
+    }
+    ingredients.html(blackBox)
+}
+async function getCategories()
+{
+    let fetchedCategories = await fetch("https://www.themealdb.com/api/json/v1/1/categories.php");
+    let response = await fetchedCategories.json();
+
+    console.log(response);
+    
+    let blackBox = ``;
+    for (let i = 0; i < response.categories.length; i++) {
+        blackBox += `<div class="col-md-3">
+                <div class="card position-relative border-0 overflow-hidden bg-transparent">
+                    <img src="${response.categories[i].strCategoryThumb}" class="h-100" alt="">
+                    <div
+                        class="layer position-absolute d-flex flex-wrap  justify-content-center align-content-start text-center ">
+                        <h3 class="text-black mb-3 mt-2">${response.categories[i].strCategory}</h3>
+                        <p>${response.categories[i].strCategoryDescription.split(" ").slice(0, 20).join(" ")}</p>
+                    </div>
+                </div>
+            </div>`;
+        
+    }
+    categories.html(blackBox)
+}
+
+
+getAllMeals();
+getCategories();
+getArea();
+getIngredients()
