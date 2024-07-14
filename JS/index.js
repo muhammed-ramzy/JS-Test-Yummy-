eb1d98c47930250613ae49e2c542fe1b7b01489c
muhammed-ramzy <muhammedabdelfattah89@gmail.com>
2024-07-14 16:47:29 +0300

    search input by name enabled

--- JS/index.js
+++ JS/index.js
@@ -2,6 +2,7 @@ let meals = $("#meals-section")
 let categories = $("#categories-section")
 let area = $("#area-section")
 let ingredients = $("#ingredients-section")
+let searchedItems = $("#searched-items-section")
 
 // Search input
 let mealNameInput = $("#meal-name-input")
@@ -9,13 +10,17 @@ let mealFLInput = $("#meal-fl-input")
 
 console.log(mealNameInput);
 
+
+
+
+// console.log(mealFLInput);
+
 let navListItems = $(".nav-list li")
 
 
 
 //navigating using the navigation list
-navListItems.on("click", (eventInfo) =>
-{
+navListItems.on("click", (eventInfo) => {
     //get the id of each one
     //add # and -section to it
     let sectionId = "#" + $(eventInfo.target).attr("id") + "-section";
@@ -27,8 +32,7 @@ navListItems.on("click", (eventInfo) =>
 
     //addClass d-none to the rest sections
     for (let i = 0; i < navListItems.length; i++) {
-        if(`#${navListItems.eq(i).attr("id")}-section` !== sectionId)
-        {
+        if (`#${navListItems.eq(i).attr("id")}-section` !== sectionId) {
             $(`#${navListItems.eq(i).attr("id")}-section`).addClass("d-none")
         }
     }
@@ -36,6 +40,10 @@ navListItems.on("click", (eventInfo) =>
     ToggleSideBar();
 })
 
+//search input by name listener
+mealNameInput.on("input", () => {
+    getSearchedMealByName(mealNameInput.val());
+})
 
 
 // side bar toggle
@@ -43,8 +51,7 @@ $("#burger-icon").on("click", () => {
     ToggleSideBar()
 })
 
-function ToggleSideBar()
-{
+function ToggleSideBar() {
     if ($("aside").css("left") == '-259.2px') {
         $("aside").animate({ "left": "0rem" }, () => {
             $("#search").animate({ "top": "0px" }, 500)
@@ -70,14 +77,40 @@ function ToggleSideBar()
 }
 
 //The main API
-async function getAllMeals()
-{
-    let fetchedMeals = await fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=");
+async function getAllMeals() {
+    let fetchedMeals = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=`);
     let response = await fetchedMeals.json();
 
+    let length = response.meals.length <= 20 ? response.meals.length : 20;
+
     let blackBox = ``;
-    for (let i = 0; i < 20; i++) {
+    for (let i = 0; i < length; i++) {
         blackBox += `<div class="col-md-3">
+                <div class="card position-relative border-0 overflow-hidden">
+                    <img src="${response.meals[i].strMealThumb
+            }" class="h-100" alt="">
+                    <div class="layer position-absolute d-flex align-items-center">
+                        <h3 class="text-black m-0">${response.meals[i].strMeal}</h3>
+                    </div>
+                </div>
+            </div>`
+
+    }
+    meals.html(blackBox)
+}
+async function getSearchedMealByName(term = '') {
+    let fetchedMeals = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`);
+    let response = await fetchedMeals.json();
+    let blackBox = ``;
+    let length;
+
+    if (fetchedMeals.ok) {
+        if (response.meals != null && response.meals.hasOwnProperty('length')) {
+
+            length = response.meals.length <= 20 ? response.meals.length : 20;
+
+            for (let i = 0; i < length; i++) {
+                blackBox += `<div class="col-md-3">
                 <div class="card position-relative border-0 overflow-hidden">
                     <img src="${response.meals[i].strMealThumb
                     }" class="h-100" alt="">
@@ -86,34 +119,44 @@ async function getAllMeals()
                     </div>
                 </div>
             </div>`
-        
+
+            }
+
+            searchedItems.html(blackBox)
+        }
+        else {
+            searchedItems.html("")
+        }
+
     }
-    meals.html(blackBox)
+
 }
-async function getArea()
-{
+async function getArea() {
     let fetchedCategories = await fetch("https://www.themealdb.com/api/json/v1/1/list.php?a=list");
     let response = await fetchedCategories.json();
-    
+
+    let length = response.meals.length <= 20 ? response.meals.length : 20;
+
     let blackBox = ``;
-    for (let i = 0; i < response.meals.length; i++) {
+    for (let i = 0; i < length; i++) {
         blackBox += `<div class="col-md-3">
                 <div class="card bg-transparent border-0 d-flex justify-content-center align-items-center text-white">
                     <i class="fa-solid fa-house-laptop fa-4x"></i>
                     <h3>${response.meals[i].strArea}</h3>
                 </div>
             </div>`;
-        
+
     }
     area.html(blackBox)
 }
-async function getIngredients()
-{
+async function getIngredients() {
     let fetchedIngredients = await fetch("https://www.themealdb.com/api/json/v1/1/list.php?i=list");
     let response = await fetchedIngredients.json();
-    
+
+    let length = response.meals.length <= 20 ? response.meals.length : 20;
+
     let blackBox = ``;
-    for (let i = 0; i < 20; i++) {
+    for (let i = 0; i < length; i++) {
         blackBox += `<div class="col-md-3">
                 <div
                     class="card bg-transparent border-0 d-flex justify-content-center align-items-center text-white text-center">
@@ -122,17 +165,18 @@ async function getIngredients()
                     <p>${response.meals[i].strDescription.split(" ").slice(0, 20).join(" ")}</p>
                 </div>
             </div>`;
-        
+
     }
     ingredients.html(blackBox)
 }
-async function getCategories()
-{
+async function getCategories() {
     let fetchedCategories = await fetch("https://www.themealdb.com/api/json/v1/1/categories.php");
     let response = await fetchedCategories.json();
-    
+
+    let length = response.categories.length <= 20 ? response.categories.length : 20;
+
     let blackBox = ``;
-    for (let i = 0; i < response.categories.length; i++) {
+    for (let i = 0; i < length; i++) {
         blackBox += `<div class="col-md-3">
                 <div class="card position-relative border-0 overflow-hidden bg-transparent">
                     <img src="${response.categories[i].strCategoryThumb}" class="h-100" alt="">
@@ -143,7 +187,7 @@ async function getCategories()
                     </div>
                 </div>
             </div>`;
-        
+
     }
     categories.html(blackBox)
 }
